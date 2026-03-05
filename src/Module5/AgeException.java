4196aa5dc8fa5c34b3143d679ebed026fad913e4
Pramod935 <pramodshetty847@gmail.com>
2026-03-05 08:58:43 +0530

    hi

---
+++ src/Module5/AgeException.java
@@ -0,0 +1,15 @@
+package Module5;
+
+
+public class AgeException {
+    static void validate(int age) {
+        if (age < 18)
+            throw new ArithmeticException("Not eligible to vote");
+        else
+            System.out.println("Eligible to vote");
+    }
+
+    public static void main(String[] args) {
+        validate(18);
+    }
+}
